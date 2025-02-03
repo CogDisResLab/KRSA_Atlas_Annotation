9a9dc8ea204f5a1d29b7a811c38e6167fea34043
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-02-03 17:02:07 -0500

    feat: update the stk sequence site generator
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- prepare_stk_sequences.R
+++ prepare_stk_sequences.R
@@ -1,12 +1,13 @@
 # Extract, clean and prepare the peptide sequences
 
-library(tidyverse)
-library(stringi)
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(stringi)
+})
 
 layout <- read_tsv("raw/87102_Array_Layout.txt")
 
 prepare_sequence <- function(seq) {
-
   modified <- str_locate_all(seq, "S|T") |>
     as.data.frame() |>
     mutate(
@@ -29,8 +30,8 @@ prepare_sequence <- function(seq) {
 
 final_list <- layout |>
   select(ID, Sequence) |>
-  filter(!is.na(Sequence), str_detect(ID, '^p', negate = TRUE)) |>
+  filter(!is.na(Sequence), str_detect(ID, "^p", negate = TRUE)) |>
   mutate(modified = map(Sequence, ~ prepare_sequence(.x))) |>
   unnest(modified) |>
   select(-start, -end, -char, -replacement) |>
-  write_csv("data/input_sequence_data.csv")
+  write_csv(file.path("data", "stk_input_sequence_data.csv"))
