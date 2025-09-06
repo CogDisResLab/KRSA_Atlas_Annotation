4de36ec300e5d0d79e4b33f5cdafd52ee740bb81
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-09-05 21:51:41 -0400

    feat: update data retrieval and refresh it
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- download_specificity_data.R
+++ download_specificity_data.R
@@ -1,84 +1,108 @@
 # Get Data from the site
 
 suppressPackageStartupMessages({
-  library(tidyverse)
-  library(httr)
-  library(jsonlite)
+    library(tidyverse)
+    library(httr2)
 })
 
-template <-
-  "https://kinase-library.phosphosite.org/api/scorer/score-site/{modified}/200/OCHOA/true/false/true"
+data <- read_csv(file.path("data", "input_sequence_data.csv"))
 
-data <- read_csv(file.path("data", "input_sequence_data.csv")) |>
-  mutate(request = str_glue(template, modified = prepared_sequence))
+process_request <- function(id, peptide_id, sequence, chip) {
+    output_file <- file.path(
+        "data",
+        "individual",
+        chip,
+        peptide_id,
+        str_glue("{id}.csv")
+    )
 
-process_request <- function(peptide_id, sequence, chip, url) {
-  output_file <- file.path(
-    "data",
-    "individual",
-    chip,
-    str_glue("{peptide_id}.csv")
-  )
+    dir.create(dirname(output_file), recursive = TRUE, showWarnings = FALSE)
 
-  dir.create(dirname(output_file), recursive = TRUE, showWarnings = FALSE)
+    if (file.exists(output_file)) {
+        message(str_glue("Data for {peptide_id} already downloaded"))
 
-  col_spec <- cols(
-    .default = col_character(),
-    sitePosition = col_double(),
-    score = col_double(),
-    scoreQuantile = col_double(),
-    scorePosition = col_double(),
-    scoreDistributionSize = col_double(),
-    scoreRank = col_double(),
-    quantileRank = col_double(),
-    id = col_double(),
-    weakCatalyticActivity = col_logical()
-  )
-
-  if (!file.exists(output_file)) {
-    message(str_glue("Downloading data for {peptide_id}"))
-    Sys.sleep(1)
-    res <- GET(url)
-
-    data <- content(res, as = "text", encoding = "utf8") |>
-      fromJSON() |>
-      as_tibble() |>
-      unnest(scores) |>
-      unnest(motif) |>
-      mutate(
-        peptide_id = peptide_id,
-        sequence = sequence
-      ) |>
-      select(-visibility) |>
-      write_csv(output_file)
-  } else {
-    message(str_glue("Data for {peptide_id} already downloaded"))
-    data <-
-      read_csv(output_file,
-        col_types = col_spec,
-        show_col_types = FALSE
-      )
+        read_csv(output_file,
+            show_col_types = FALSE
+        )
+    } else {
+        message(str_glue("Downloading data for {id}"))
+        user_agent <- str_glue("KRSA Annotation; https://github.com/CogDisResLab/stk_annotation_update; {gitr::gitr_current_sha()} ")
+        Sys.sleep(1L)
+        graphql_url <- "https://kinase-library.phosphosite.org/kinase-library-api/graphql"
+        graphql_query <- "
+            query ScoreSite($sequence: String!,
+            $pp: Boolean,
+            $non_canonical: Boolean) {
+  scoreSite(sequence: $sequence, pp: $pp, nonCanonical: $non_canonical) {
+    processedSequence
+    kinases {
+      family
+      geneName
+      name
+      percentile
+      percentileRank
+      score
+      scoreRank
+      uniprotId
+      displayName
+    }
+    __typename
   }
+}"
+
+        graphql_vars <- list(
+            sequence = sequence,
+            pp = TRUE,
+            non_canonical = FALSE
+        )
+        req <- request(graphql_url) |>
+            req_headers(
+                "Content-Type" = "application/json", # nolint: nonportable_path_linter.
+            ) |>
+            req_body_json(list(
+                query = graphql_query,
+                variables = graphql_vars,
+                operationName = "ScoreSite"
+            )) |>
+            req_user_agent(user_agent)
+        res <- req_perform(req)
+        resp <- resp_body_json(res, simplifyVector = TRUE)
+        # Defensive: check for errors and expected structure
+        if (!is.null(resp[["errors"]])) {
+            stop("GraphQL error:", resp[["errors"]][[1L]][["message"]],
+                call. = FALSE
+            )
+        }
+        site <- resp[["data"]][["scoreSite"]]
+        kinases <- site[["kinases"]]
+        kinases |>
+            mutate(
+                id = id,
+                peptide_id = peptide_id,
+                sequence = sequence,
+                processed_sequence = site[["processedSequence"]]
+            ) |>
+            write_csv(output_file)
+    }
 }
 
 all_data <- data |>
-  select(ID, prepared_sequence, chip_type, request) |>
-  pmap_dfr(~ process_request(..1, ..2, ..3, ..4)) |>
-  write_csv("results/complete_kinase_specificity_map_raw.csv.gz") |>
-  select(
-    peptide_id,
-    sequence,
-    siteLabel,
-    kinase_name = name,
-    gene_name = geneName,
-    kinase_group = motifGroup,
-    kinase_type = type,
-    score,
-    score_quantile = scoreQuantile,
-    score_rank = scoreRank,
-    quantile_rank = quantileRank,
-    score_position = scorePosition,
-    score_distribution_size = scoreDistributionSize,
-    weak_activity = weakCatalyticActivity
-  ) |>
-  write_csv("results/complete_kinase_specificity_map.csv.gz")
+    select(ID, PeptideID, prepared_sequence, chip_type) |>
+    pmap(~ process_request(..1, ..2, ..3, ..4)) |>
+    bind_rows() |>
+    write_csv(file.path("results", "complete_kinase_specificity_map_raw.csv.gz")) |>
+    select(
+        ID = id,
+        PeptideID = peptide_id,
+        SourceSequence = sequence,
+        ProcessedSequence = processed_sequence,
+        Family = family,
+        GeneName = geneName,
+        DisplayName = displayName,
+        Percentile = percentile,
+        PercentileRank = percentileRank,
+        Score = score,
+        ScoreRank = scoreRank,
+        UniprotID = uniprotId
+    ) |>
+    write_csv(file.path("results", "complete_kinase_specificity_map.csv.gz"))
