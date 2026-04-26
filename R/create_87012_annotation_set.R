fff88898f27fa472297b65c0690b9d26fa793ebc
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2026-04-25 20:04:29 -0400

    feat: map the kinase gene names to krsa_ids

--- R/create_87012_annotation_set.R
+++ R/create_87012_annotation_set.R
@@ -1,11 +1,15 @@
 # Create an annotation for the 87012 version of the chip.
 
 library(tidyverse)
+library(creedenzymatic)
 
 data_files <- list.files(file.path("data", "individual", "STK", "ART87012"),
     recursive = TRUE,
     full.names = TRUE
 )
+
+kinome_mapping <- kinome_mp_file |>
+    select(geneName = hgnc_symbol, krsa_id)
 
 # Load the simple annotation data into one frame
 annotation_data <- data_files |>
@@ -16,8 +20,8 @@ annotation_data <- data_files |>
 
 # Diagnostics for each peptide by kinase and annotations
 clean_annotation_data <- annotation_data |>
-    select(Peptide, displayName, scoreRank) |>
-    summarise(scoreRank = mean(scoreRank), .by = c(Peptide, displayName)) |>
+    select(Peptide, geneName, scoreRank) |>
+    summarise(scoreRank = mean(scoreRank), .by = c(Peptide, geneName)) |>
     nest(.by = Peptide) |>
     mutate(
         n_kinase = map_dbl(data, nrow),
@@ -29,20 +33,23 @@ clean_annotation_data <- annotation_data |>
 # Clean the dataset by averaging multiple phosphosites
 # and selecting the relevant columsn
 clean_annotation_data <- annotation_data |>
-    select(Peptide, displayName, scoreRank) |>
-    summarise(scoreRank = mean(scoreRank), .by = c(Peptide, displayName)) |>
+    select(Peptide, geneName, scoreRank) |>
+    summarise(scoreRank = mean(scoreRank), .by = c(Peptide, geneName)) |>
     group_by(Peptide) |>
     arrange(scoreRank, .by_group = TRUE) |>
     slice_min(order_by = scoreRank, prop = 0.25, with_ties = TRUE) |>
-    ungroup()
+    ungroup() |>
+    inner_join(kinome_mapping, by = c("geneName")) |>
+    filter(krsa_id != "NA") |>
+    unique()
 
 annotation_mapping <- clean_annotation_data |>
-    select(-scoreRank) |>
+    select(-scoreRank, -geneName) |>
     nest(.by = Peptide) |>
     mutate(
         Kinase = map(data, \(df) {
             df |>
-                pull(displayName) |>
+                pull(krsa_id) |>
                 str_flatten(collapse = " ")
         })
     ) |>
@@ -54,7 +61,7 @@ annotation_mapping |>
     write_csv("results/ART87012-STK-Mapping.csv")
 
 annotation_coverage <- clean_annotation_data |>
-    select(Substrates = Peptide, Kin = displayName) |>
+    select(Substrates = Peptide, Kin = krsa_id) |>
     unique()
 
 annotation_coverage |>
